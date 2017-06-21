2b960757ef4204da66302a68fa8e0622a9f4fabf
= <=>
2017-06-20 23:29:50 -0700

    First test of execlp

--- program1/processes.cpp
+++ program1/processes.cpp
@@ -1,10 +1,47 @@
-# CSS 430 Program 1
+// CSS 430 Program 1
 
+#include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
-#include <stdio>
+#include <sys/wait.h>
+#include <iostream>
 
 using namespace std;
 
-int main(){
+/*  Emulates `ps -A | grep argv[1] | wc - l` using fork(), execlp(), pipe(),
+ *  etc.
+ *
+ */
+
+int main(int argc, char *argv[]){
+
+    // 1) Handle input
+
+    cout << argv[1] << endl;
+
+    // 2) Set up pipe for parent/child1
+
+    // 3) Spawn first child
+
+    // 3.A) if first child
+
+    execlp("/bin/ps", "ps", "-A"); 
+            // A.1) Set up pipe for child/grandchild
+
+            // A.2) Spawn grandchild
+
+            // A.3.a) if grandchild
+
+                // a.1) set up pipe for grandchild/great-grandchild
+
+                // a.2) spawn great-grandchild
+
+                // a.2.i) if great-grandchild
+
+                // a.2.ii) else (grandchild)
+
+            // A.3.b) else (child)
+
+    // 3.B) else (parent)
 	
 };
