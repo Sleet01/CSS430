0ec86b24b2792eaa24035185b4856ff0659800a7
= <=>
2017-06-25 13:44:08 -0700

    Cleaned up all output Added doxygen function blocks x

--- program1/processes.cpp
+++ program1/processes.cpp
@@ -11,13 +11,19 @@ using namespace std;
 
 /*  @brief  Helper that wraps pipe-getting logic and reports errors
  *  @pre    fd is declared in the calling method
- *  @post   fd 
+ *  @post   fd is modified with new file descriptors, or unmodified 
+ *          (on failure).
+ *  @param[out]     fd      int [] which will hold pipe FD numbers
+ *  @return         got     true if pipe assigned; false if not
  */
 bool getPipe(int fd[2]){
     
     bool got = false;
     
+    // pipe() syscall returns -1 if failed, 0 otherwise
     if (pipe(fd) < 0){
+        
+        // print relevant error information
         perror("Pipe error:");
     }
     else{
@@ -27,21 +33,28 @@ bool getPipe(int fd[2]){
     return got;
 }
 
-/*  Emulates `ps -A | grep argv[1] | wc -l` using fork(), execlp(), pipe(),
- *  etc.
- *
+/*  @brief  Emulates `ps -A | grep argv[1] | wc -l` using fork(), execlp(), pipe(),
+ *          etc.
+ *  @pre    this file is compiled and instantiated from a POSIX-compatible
+ *          shell (bash, a Cygwin terminal, etc.)
+ *  @post   N/A
+ *  @param[in]  argc        count of command-line args.  This file's name is #0
+ *  @param[in]  argv        char * array of CLI args.  This file's name is 
+ *                          argv[0]
+ *  @return     
  */
 int main(int argc, char *argv[]){
 
     // 1) Handle input
-
     if(argc != 2){
-        cout << "Incorrect number of arguments; aborting!" << endl;
-        return 1;
+        cerr << "Incorrect number of arguments; aborting!" << endl;
+        return EXIT_FAILURE;
     }
+
     // strcmp returns 0 if strings match, which is boolean False
     else if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")){
-        cout << argv[1] << endl;
+        //Logging
+        //cout << argv[1] << endl;
         cout << "Usage: \'processes <search term> | -h/--help\'" << endl << endl;
         return 0;
     }
@@ -50,7 +63,8 @@ int main(int argc, char *argv[]){
     string argument1(argv[1]);
     string fullCMD = "ps -A | grep " + argument1 + " | wc -l";
 
-    cout << "Running full command: " << fullCMD << endl;
+    // Logging
+    //cout << "Running full command: " << fullCMD << endl;
 
     // 2) Prep for and Spawn first child
     int fd[2];
@@ -59,89 +73,107 @@ int main(int argc, char *argv[]){
     // Basic fork() error checking
     if((pid = fork()) < 0){
         perror("Fork error:");
-        exit(2);
-    }
-    // 2.A) if first child
+        return EXIT_FAILURE;
+    } // 2.A) if first child
     else if (pid == 0){
+        
+        // This makes pipe and FD addressing cleaner
         enum {RD, WR};
 
         // A.1) Set up pipe for child/grandchild
         // If we don't get a pipe, exit!
         if (!getPipe(fd)){
-            exit(1);
+            return EXIT_FAILURE;
         }
 
         // A.2) Spawn grandchild
+        // If fork fails, exit!
         if((pid = fork()) < 0){
-            perror("Fork error:");
-            exit(2);
-        }
-        // A.3.a) if grandchild
+            perror("Fork error (grandchild):");
+            return EXIT_FAILURE;
+        } // A.3.a) if grandchild
         else if (pid == 0){
 
             // a.1) set up pipe for grandchild/great-grandchild
+            // We can re-use pid, but the two pipes must exist simultaneously
+            // so a second pipe must be defined.
             int fdgc[2];
+
             // If we don't get a pipe, exit!
             if (!getPipe(fdgc)){
-                exit(1);
+                return EXIT_FAILURE;
             }
 
             // a.2) spawn great-grandchild
+            // Again, if forking fails, exit!
             if((pid = fork()) < 0){
                 perror("Fork error (great-grandchild):");
-                exit(2);
-            }
-            // a.2.i) if great-grandchild
+                return EXIT_FAILURE;
+            }// a.2.i) if great-grandchild
             else if (pid == 0){
-                cout << "Great-grandchild: executing partial command: ps -A" << endl;
+
+                // Logging
+                //cout << "Great-grandchild: executing partial command: ps -A" << endl;
+
                 // Redirect pipe in accordance with Program1 Part1 specs
                 // Close Great-GrandChild's RD end of Great-Grandchild/GrandChild pipe
                 close(fdgc[RD]);
+
                 // Close Great-GrandChild's connection to stdout
                 close(WR);
+                
                 // Reassign the pipe's WR end to stdout fd
                 dup2(fdgc[WR], WR);
+
                 // Execute 'ps -a'
                 execlp("ps", "ps", "-A", (char*)NULL);
 
-            // a.2.ii) else (grandchild)
-            }
+            } // a.2.ii) else (grandchild)
             else{
+
                 // Logging
-                cout << "Grandchild: executing partial command: grep " << argument1 << endl;
+                //cout << "Grandchild: executing partial command: grep " << argument1 << endl;
+
                 // Redirect pipe in accordance with Program1 Part1 specs
                 // Close Grandchild's RD end of the Grandchild/Child pipe.
                 close(fd[RD]);
+
                 // Close Grandchild's WR end of the Great-Grandchild/Grandchild pipe.
                 close(fdgc[WR]);
 
+
                 // Close GrandChild's connection to stdin.
                 close(RD);
+
                 // Close GrandChild's connection to stdout.
                 close(WR);
+
                 
                 // Reassign the child/grandchild pipe's WR end to stdout fd
                 dup2(fd[WR], WR);
+
                 // Reassign the grandchild/great-grandchild pipe's RD end to stdin fd
                 dup2(fdgc[RD], RD);
 
-                // cerr << "DEBUG: grandchild's copy of ARGV[1]: " << argument1 << endl;
-                // cerr << "DEBUG: copy of ARGV[1] == 'kwork': " << ((!strcmp(argv[1], argument1.c_str())) ? "True" : "False" ) <<
-                //    endl;
-
+                // execlp only passes char * [] arguments, but argument1 is now
+                // a c++ string, so we grab the underlying cstring to pass to execlp
                 execlp("grep", "grep", argument1.c_str(),(char*)NULL); 
             }
 
         } 
         // A.3.b) else (child)
         else {
+
             // Logging
-            cout << "Child: Executing partial command: wc -l" << endl;
+            //cout << "Child: Executing partial command: wc -l" << endl;
+
             // Redirect pipe in accordance with Program1 Part1 specs
-            // Close Child1's Write end of the pipe.
+            // Close Child1's WR end of the pipe.
             close(fd[WR]);
+
             // Close Child1's connection to stdin
             close(RD);
+
             // Reassign the pipe's RD end to stdin fd
             dup2(fd[RD], RD);
                     
@@ -151,7 +183,13 @@ int main(int argc, char *argv[]){
     } 
     // 2.B) else (parent)
     else {
+
+        // Per Program 1 Part 1 spec, the parent process must wait for all children to complete. 
         wait(NULL);
-        cout << "Parent completed!" << endl;
+
+        // Logging
+        //cout << "Parent completed!" << endl;
+
+        return EXIT_SUCCESS;
 	}
 };
