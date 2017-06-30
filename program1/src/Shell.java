e807054c848a7079fd604523cb38df8da6d96910
= <=>
2017-06-29 17:42:15 -0700

    Initial Shell.java layout

--- program1/src/Shell.java
+++ program1/src/Shell.java
@@ -8,14 +8,32 @@
 public class Shell extends Thread {
 
     public Shell()  {
+        // set up regex pattern
+    
     }
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        Shell shell = new Shell();
-        shell.run();
+    public void run() {
+
+        private int cmdNum = 1;
+        
+        while(true){
+        
+        // output prompt
+
+        // take in user input
+
+        // if input == "exit": exit
+
+        // split input on ' ; ' (or something similar)
+
+        // For ;-delimited command line
+
+            // for every &-delimited command
+
+                // record tid from exec(command)
+
+        // After last command, do "while( SysLib.join() != tid ){}"
+
+        }
     }
 }
