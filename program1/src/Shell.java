9540304235d71bbd5bbc77c09bfc9ef6be8f8930
Sleet01 <sleet01@hotmail.com>
2017-06-29 20:06:09 -0700

    Filled in most code
    Need to add:
    - Actual execution - needs tid
    - Edge cases
    -- Null command
    -- Exit

--- program1/src/Shell.java
+++ program1/src/Shell.java
@@ -1,39 +1,63 @@
 /**
- * @author !@#$%%$#@!!@#$%%$#@!      PUT YOUR NAME HERE !@#$%%$#@!!@#$%%$#@!!@#$%%$#@!
+ * @author Martin L. Metke
  *
- * There's a separate folder with the ThreadOS .class files
  *
  */
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+
 public class Shell extends Thread {
 
+    private int cmdNumber;
+    private Pattern seqDelimiter;
+    private Pattern consecDelimiter;
+
     public Shell()  {
-        // set up regex pattern
-    
+
+        // Set up cmdNum starting point
+        cmdNumber = 1;
+
+        // set up regex patterns
+        seqDelimiter = Pattern.compile(" +;+ +");
+        consecDelimiter = Pattern.compile(" +&+ +");
     }
 
     public void run() {
 
-        private int cmdNum = 1;
-        
+        StringBuffer userCommand = new StringBuffer("");
+        String [] sequentialChunks;
+        String [] consecutiveChunks;
+
+        // Enter a continuous loop of reading user input and executing commands
         while(true){
         
-        // output prompt
+            // output prompt
+            SysLib.cout("Shell[" + cmdNumber++ + "]% ");
 
-        // take in user input
+            // take in user input (continuously re-check until length of cin > 0
+            while ( SysLib.cin(userCommand) <= 0);
 
-        // if input == "exit": exit
+            // split input on ' ; ' (or something similar)
+            // Splits the user input (cast into a string) into a string array
+            sequentialChunks = seqDelimiter.split( userCommand.toString() );
 
-        // split input on ' ; ' (or something similar)
+            // For ;-delimited command line
+            for( String chunk: sequentialChunks ) {
 
-        // For ;-delimited command line
+                consecutiveChunks = consecDelimiter.split(chunk);
 
-            // for every &-delimited command
+                // for every &-delimited command
+                for( String command: consecutiveChunks ) {
 
-                // record tid from exec(command)
+                    // if input == "exit": exit
 
-        // After last command, do "while( SysLib.join() != tid ){}"
+                    // else record tid from exec(command)
+                }
 
+                // After last command, do "while( SysLib.join() != tid ){}"
+            }
         }
     }
 }
