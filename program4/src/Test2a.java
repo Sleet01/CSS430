0ee59df6e286f12c65f52dd84a255ebee7667217
Sleet01 <sleet01@hotmail.com>
2017-07-28 16:11:09 -0700

    Prelim for Assignment 4
    Basically just 2 + Delta.

---
+++ program4/src/Test2a.java
@@ -0,0 +1,19 @@
+class Test2a extends Thread {
+
+    public void run( ) {
+	String[] args1 = SysLib.stringToArgs( "TestThread2b a 500" );
+//	String[] args2 = SysLib.stringToArgs( "TestThread2b b 1000" );
+//	String[] args3 = SysLib.stringToArgs( "TestThread2b c 3000" );
+//	String[] args4 = SysLib.stringToArgs( "TestThread2b d 6000" );
+//	String[] args5 = SysLib.stringToArgs( "TestThread2b e 500" );
+	SysLib.exec( args1 );
+//	SysLib.exec( args2 );
+//	SysLib.exec( args3 );
+//	SysLib.exec( args4 );
+//	SysLib.exec( args5 );
+	for (int i = 0; i < 1; i++ )
+	    SysLib.join( );
+	SysLib.cout( "Test2a finished\n" );
+	SysLib.exit( );
+    }
+}
