127353cdb1417e62e5ee84c63bd814190c80e2f6
Sleet01 <sleet01@hotmail.com>
2017-07-31 16:22:12 -0700

    Started work on block entry and constructor
    It appears that A) this assignment will be very easy, and B) there is
    much room for improvement with this cache design.

--- program4/src/Cache.java
+++ program4/src/Cache.java
@@ -9,14 +9,23 @@ import java.util.concurrent.*;
  */
 public class Cache {
 
-    private Entry[] pageTable = null;
+    private static final int CACHE_SIZE = 10;
 
+    private CacheEntry[] cacheTable = null;
+
+    /**
+     * @brief   Constructor for Cache class.  Creates the cache store and initializes page table
+     * @param blockSize     int, size of a disk block (and corresponding cache page)
+     * @param cacheBlocks   int, count of disk blocks / pages that can be cached at once
+     */
     public Cache(int blockSize, int cacheBlocks) {
-    }
 
-    private class Entry {
-    }
+        cacheTable = new CacheEntry[cacheBlocks];
 
+        for (int i = 0; i < cacheBlocks; ++i){
+            cacheTable[i] = new CacheEntry(blockSize);
+        }
+    }
 
     private int findFreePage() {
         return -1;
@@ -41,5 +50,22 @@ public class Cache {
     }
 
     public synchronized void flush() {
+    }
+
+    /**
+     * Inner Class with nothing but public members; essentially as Struct-y a
+     * data object as Java can encompass.
+     */
+    private class CacheEntry {
+
+        public int block = -1;
+        public boolean refbit = false;
+        public boolean dirtybit = false;
+        public byte[] data;
+
+        public CacheEntry(int blockSize){
+            data = new byte[blockSize];
+        }
+
     }
 }
