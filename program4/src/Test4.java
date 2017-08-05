ccde431d843dc636e7ed34415d5d462631ebe70e
Sleet01 <sleet01@hotmail.com>
2017-08-04 23:54:16 -0700

    Added tons of error checking.  Probably useless.
    But hey!  I'm learning... crap.

--- program4/src/Test4.java
+++ program4/src/Test4.java
@@ -4,26 +4,73 @@ import java.util.Date;
 
 class Test4 extends Thread {
 
-    public final static int OK = Kernel.OK;
-    public final static int ERROR = Kernel.ERROR;
+    private final static int OK = Kernel.OK;
+    private final static int ERROR = Kernel.ERROR;
+    private int suite;
+    private boolean caching;
+    private int instError = 0;
 
+    public Test4(){
+        this.instError = 1;
+    }
+
+    public Test4(String [] argv){
+
+        // Only accepts 2 arguments
+        if(argv.length != 2){
+            this.instError = 1;
+            return;
+        }
+
+        // Enabled or disable caching (MUST EXPLICITLY BE SET)
+        if(argv[0].toLowerCase().equals( "enabled" )){
+            this.caching = true;
+        }
+        else if (argv[0].toLowerCase().equals( "disabled" )){
+            this.caching = false;
+        }
+        else {
+            this.instError = 2;
+            return;
+        }
+
+        // Get "suite" of tests to run
+        this.suite = Integer.parseInt(argv[1]);
+        if(!(this.suite >= 0 && this.suite <= 4)){
+            this.instError = 3;
+        }
+
+    }
 
     public boolean testBadReadFails(){
         boolean result = false;
-        result = ( SysLib.cread(-1, new byte[512]) == ERROR);
-        result = result & ( SysLib.cread( 1000, new byte[512]) == ERROR);
+        if(caching) {
+            result = (SysLib.cread(-1, new byte[512]) == ERROR);
+            result = result & (SysLib.cread(1000, new byte[512]) == ERROR);
+        } else {
+            result = (SysLib.rawread(-1, new byte[512]) == ERROR);
+            result = result & (SysLib.rawread(1000, new byte[512]) == ERROR);
+        }
         return result;
     }
 
     public boolean testReadZeroSucceeds(){
         boolean result = false;
-        result = ( SysLib.cread(0, new byte[512]) == OK);
+        if(caching) {
+            result = (SysLib.cread(0, new byte[512]) == OK);
+        } else {
+            result = (SysLib.rawread(0, new byte[512]) == OK);
+        }
         return result;
     }
 
     public boolean testReadLastSucceeds(){
         boolean result = false;
-        result = ( SysLib.cread(999, new byte[512]) == OK);
+        if(caching) {
+            result = (SysLib.cread(999, new byte[512]) == OK);
+        } else {
+            result = (SysLib.rawread( 999, new byte[512]) == OK);
+        }
         return result;
     }
 
@@ -32,27 +79,68 @@ class Test4 extends Thread {
      */
     public void run(){
 
-        int count = 0;
+        switch(this.instError) {
+            case 0: break;
+            case 1: {
+                SysLib.cout("Incorrect Test4 invocation!  Use \"Test4 <enabled|disabled> <0-4>\"");
+                SysLib.exit();
+                break;
+            }
+            case 2: {
+                SysLib.cout("Invalid caching selection!  Use \"enabled\" or \"disabled\"");
+                SysLib.exit();
+                break;
+            }
+            case 3: {
+                SysLib.cout("Invalid suite selection!  Use \"0\" through \"4\"");
+                SysLib.exit();
+                break;
+            }
+        }
 
-        // Start of tests.  First test initializes "last" variable, which we'll use from here on out.
-        boolean last = testBadReadFails();
+        switch(this.suite) {
+            case 0: {
+                int count = 0;
 
-        SysLib.cout("Test 1: OOB read calls fail: " + ((last) ? "PASS" : "FAIL") + "\n" );
-        count += ((last) ? 0: 1);
+                // Start of tests.  First test initializes "last" variable, which we'll use from here on out.
+                boolean last = testBadReadFails();
 
-        last = testReadZeroSucceeds();
-        SysLib.cout("Test 2: Read from Block 0 succeeds: " + ((last) ? "PASS" : "FAIL") + "\n" );
-        count += ((last) ? 0: 1);
+                SysLib.cout("Test 1: OOB read calls fail: " + ((last) ? "PASS" : "FAIL") + "\n");
+                count += ((last) ? 0 : 1);
 
-        last = testReadLastSucceeds();
-        SysLib.cout("Test 2: Read from Block 0 succeeds: " + ((last) ? "PASS" : "FAIL") + "\n" );
-        count += ((last) ? 0: 1);
+                last = testReadZeroSucceeds();
+                SysLib.cout("Test 2: Read from Block 0 succeeds: " + ((last) ? "PASS" : "FAIL") + "\n");
+                count += ((last) ? 0 : 1);
 
-        // Output totals
-        SysLib.cout("TOTAL FAILURES: " + count + "\n");
+                last = testReadLastSucceeds();
+                SysLib.cout("Test 2: Read from Block 0 succeeds: " + ((last) ? "PASS" : "FAIL") + "\n");
+                count += ((last) ? 0 : 1);
+
+                // Output totals
+                SysLib.cout("TOTAL FAILURES: " + count + "\n");
+
+                break;
+
+            }
+            case 1: {
+                SysLib.cout("This suite is not yet implemented!");
+                break;
+            }
+            case 2: {
+                SysLib.cout("This suite is not yet implemented!");
+                break;
+            }
+            case 3: {
+                SysLib.cout("This suite is not yet implemented!");
+                break;
+            }
+            case 4: {
+                SysLib.cout("This suite is not yet implemented!");
+                break;
+            }
+        }
 
         // Exit cleanly
         SysLib.exit();
     }
-
 }
